7e004f1c2dd7fa0971a7d078b10e1723fe55d0e9
SandersJ <sandersjack141@gmail.com>
2021-09-17 14:16:50 +0100

    SPC

--- simulationg4/SPC_Simulation/GasFiles/GasPlots/env.sh
+++
@@ -1,5 +0,0 @@
-source /cvmfs/sft.cern.ch/lcg/contrib/gcc/9.1.0/x86_64-centos7-gcc9-opt/setup.sh
-
-export GARFIELD_HOME=/home/$(whoami)/Software/garfieldpp
-export HEED_DATABASE=$GARFIELD_HOME/Heed/heed++/database/
-
